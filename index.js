548d1d3891356926a267799ab8f6f711322e5061
sahinerbay <sahin@sahinerbay.com>
2017-10-28 22:13:14 +0300

    test

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
-const app = express()
+const app = express();
+
+var port = process.env.PORT || 8080;
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(port, function () {
+  console.log(`Example app listening on port ${port}!`)
 });
